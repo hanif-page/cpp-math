050b9682dbda638b0c8d5d476deb90593931b70a
hanif-sudoapt <amrhanif15@gmail.com>
2021-01-15 15:46:25 +0700

    make a new factorial counter, on this calculator

--- calculator/calculator.cpp
+++ calculator/calculator.cpp
@@ -3,11 +3,23 @@
 
 using namespace std;
 
+double factorial(double num){
+    double result = 1;
+
+    for(int i = 1; i <= num; i++){
+        result = result * i;
+    }
+    
+    return result;
+}
+
 int main()
 {
     int num1, num2;
     char op;
 
+
+    cout << "(If you want to search a factorial, just enter the operator with exclamation mark, and enter the second number with 0" << endl << endl;
     cout << "Enter first number: ";
     cin >> num1;
 
@@ -34,6 +46,9 @@ int main()
     }
     else if (op == '%'){
         cout << num1 << " " << op << " " << num2 << " = " << num1 % num2 << endl;
+    }
+    else if(op == '!'){
+        cout << num1 << op << " = " << factorial(num1) << endl;
     }
 
 
--- calculator/calculator.d
+++ calculator/calculator.d
@@ -1,2 +1,2 @@
 obj/calculator.o: \
- /home/hanif/Documents/coding/cpp/vscode-cpp/cpp-math/calculator/calculator.cpp
+ /home/hanif/Documents/coding/git-github/cpp-math/calculator/calculator.cpp
